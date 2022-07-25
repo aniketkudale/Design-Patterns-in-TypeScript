9a3ddf2d3e0dfa33eb3546740b796017eee524e8
Aniket Kudale <aniket.kudale@hotmail.com>
2022-07-25 19:41:44 +0530

    Added Singleton Pattern

---
+++ Creational-Patterns/singleton.ts
@@ -0,0 +1,45 @@
+/**
+ * Singleton class defines getInstance method, which allows client to acces the unique singleton instance
+ */
+class Singleton {
+
+    private static instance: Singleton;
+    /**
+     * Making contstructor private so that direct calls to constructor can be avoided, using 'new'
+     */
+    private constructor() {}
+
+    /**
+     * This static method controls the access to Singleton instance
+     */
+    public static getInstance(): Singleton {
+        if(!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    }
+
+    /**
+     * Any Singleton will have some business logic which can be executed on its instance
+     */
+
+    public businessLogic() {
+        //
+    }
+}
+
+/**
+ * Client Code
+ */
+
+function clietCode() {
+    const s1 = new Singleton.getInstance();
+    const s2 = new Singleton.getInstance();
+
+    if ( s1=== s2) {
+        console.log('There is only one instance');
+    } else {
+        console.log('There are two different Singleton instance');
+    }
+
+}
