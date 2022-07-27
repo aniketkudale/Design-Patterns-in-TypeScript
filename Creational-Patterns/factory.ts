9824009973b4a3d58a46616e79e5560a2803a228
Aniket Kudale <aniket.kudale@hotmail.com>
2022-07-27 23:13:19 +0530

    Refactored

--- Creational-Patterns/factory.ts
+++ Creational-Patterns/factory.ts
@@ -5,7 +5,7 @@ interface Product {
 abstract class Creator {
     public abstract factoryMethod(): Product;
     public someOperation(): string {
-        const product = this .factoryMethod();
+        const product = this.factoryMethod();
         return `Creator: The same creator's code has just worked with ${product.operation()}`;
     }
 }
